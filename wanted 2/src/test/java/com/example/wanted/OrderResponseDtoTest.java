a6296f29d8dfec74b48d59f15e3f64742cb5b1af
mkch0 <mk.cho3221@gmail.com>
2024-07-31 19:44:46 +0900

    test: orderResponseDto 테스트 transaction 적용

--- wanted 2/src/test/java/com/example/wanted/OrderResponseDtoTest.java
+++ wanted 2/src/test/java/com/example/wanted/OrderResponseDtoTest.java
@@ -1,17 +1,18 @@
 package com.example.wanted;
 
 import com.example.wanted.member.Member;
+import com.example.wanted.member.MemberRepository;
 import com.example.wanted.member.MemberService;
-import com.example.wanted.order.Order;
-import com.example.wanted.order.OrderMapper;
-import com.example.wanted.order.OrderPostDto;
-import com.example.wanted.order.OrderResponseDto;
+import com.example.wanted.order.*;
 import com.example.wanted.product.Product;
+import com.example.wanted.product.ProductRepository;
 import com.example.wanted.product.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,35 +24,45 @@ public class OrderResponseDtoTest {
     private MemberService memberService;
     @Autowired
     private ProductService productService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @BeforeEach
+    public void setUp(){
+        orderRepository.deleteAll();
+        productRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
 
     @DisplayName("OrderResponseDto 확인1")
     @Test
+    @Transactional
     public void testOrderToOrderResponseDto(){
         //get
         Member seller = new Member();
-        seller.setMemberId(1L);
         seller.setEmail("123@seller.com");
         seller.setMemberName("Seller");
         seller.setPassword("sell123");
 
-        memberService.createMember(seller);
+        Member savedSellerMember = memberService.createMember(seller);
 
         Member buyer = new Member();
-        buyer.setMemberId(2L);
         buyer.setEmail("123@buyer.com");
         buyer.setMemberName("Buyer");
         buyer.setPassword("buy123");
 
-        memberService.createMember(buyer);
-
+        Member savedBuyerMember = memberService.createMember(buyer);
 
         Product product = new Product();
-        product.setProductId(1L);
         product.setProductName("New Product");
         product.setPrice(100);
         product.setSeller(seller);
 
-        productService.createProduct(product);
+        Product savedProduct = productService.createProduct(product);
 
         Order order = new Order();
         order.addMember(buyer);
@@ -61,38 +72,36 @@ public class OrderResponseDtoTest {
         OrderResponseDto responseDto = orderMapper.orderToOrderResponseDto(order);
 
         //then
-        assertThat(responseDto.getSellerId()).isEqualTo(1L);
-        assertThat(responseDto.getBuyerId()).isEqualTo(2L);
-        assertThat(responseDto.getProductId()).isEqualTo(1L);
+        assertThat(responseDto.getSellerId()).isEqualTo(savedSellerMember.getMemberId());
+        assertThat(responseDto.getBuyerId()).isEqualTo(savedBuyerMember.getMemberId());
+        assertThat(responseDto.getProductId()).isEqualTo(savedProduct.getProductId());
     }
 
     @Test
     @DisplayName("OrderResponseDto 확인2")
+    @Transactional
     public void testOrderToOrderResponseDto2() {
         // Given
         Member seller = new Member();
-        seller.setMemberId(1L);
         seller.setEmail("123@seller.com");
         seller.setMemberName("Seller");
         seller.setPassword("sell123");
 
-        memberService.createMember(seller);
+        Member savedSellerMember = memberService.createMember(seller);
 
         Member buyer = new Member();
-        buyer.setMemberId(2L);
         buyer.setEmail("123@buyer.com");
         buyer.setMemberName("Buyer");
         buyer.setPassword("buy123");
 
-        memberService.createMember(buyer);
+        Member savedBuyerMember = memberService.createMember(buyer);
 
         Product product = new Product();
-        product.setProductId(1L);
         product.setProductName("New Product");
         product.setPrice(100);
         product.setSeller(seller);
 
-        productService.createProduct(product);
+        Product savedProduct = productService.createProduct(product);
 
         Order order = new Order();
         order.addMember(buyer);
@@ -103,7 +112,6 @@ public class OrderResponseDtoTest {
 
         // Then
         System.out.println("OrderResponseDto: " + responseDto); // 디버깅용 출력
-        assertThat(responseDto.getSellerId()).isEqualTo(1L);
+        assertThat(responseDto.getSellerId()).isEqualTo(savedSellerMember.getMemberId());
     }
-
 }
