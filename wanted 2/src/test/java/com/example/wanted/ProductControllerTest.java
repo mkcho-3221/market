6236c1e9278810d9d4927c15f84a786cc5a386b2
mkch0 <mk.cho3221@gmail.com>
2024-08-05 19:35:13 +0900

    test: 로그인 시 제품 등록 및 구매 가능 테스트 추가

---
+++ wanted 2/src/test/java/com/example/wanted/ProductControllerTest.java
@@ -0,0 +1,94 @@
+package com.example.wanted;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private String obtainAccessToken(String username,
+                                     String password) throws Exception{
+
+        System.out.println("Sending login request for username : " + username);
+        System.out.println("Sending login request for password : " + password);
+
+        MvcResult result = mockMvc.perform(post("/auth/login")
+                        .content("{\"username\" : \"testUser@email.com\" , \"password\" : \"testPassword\"}"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+
+        System.out.println("Login response : "+ response);
+
+        return response;
+    }
+
+    @Test
+    @DisplayName("로그인 테스트")
+    public void loginTest() throws Exception{
+        //member 등록
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"memberName\" : \"testUser\",\"email\":\"testUser@email.com\",\"password\":\"testPassword\"}"))
+                .andExpect(status().isCreated());
+
+        //로그인
+        String token = obtainAccessToken("testUser@email.com",
+                "testPassword");
+    }
+
+    @Test
+    @DisplayName("회원은 제품 등록과 구매가 가능함")
+    public void testMemberFunctions() throws Exception{
+        //member-seller 등록
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"memberName\" : \"testSellerUser\",\"email\":\"testSellerUser@email.com\",\"password\":\"sellerPassword\"}"))
+                .andExpect(status().isCreated());
+
+        //member-buyer 등록
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"memberName\" : \"testBuyerUser\",\"email\":\"testBuyerUser@email.com\",\"password\":\"buyerPassword\"}"))
+                .andExpect(status().isCreated());
+
+        //로그인
+        String tokenSeller = obtainAccessToken("testSellerUser@email.com",
+                "sellerPassword");
+        String tokenBuyer = obtainAccessToken("testBuyerUser@email.com",
+                "buyerPassword");
+
+        //product 등록
+        mockMvc.perform(post("/products")
+                        .header("Authorization","Bearer "+ tokenSeller)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"productName\" : \"커피\",\"productStatus\" : \"PRODUCT_AVAILABLE\",\"price\" : 500,\"seller\" : {\"memberId\" : 1, \"name\": \"Seller\" }}"))
+                .andExpect(status().isCreated());
+
+        //order 등록
+        mockMvc.perform(post("/orders")
+                .header("Authorization", "Bearer " + tokenBuyer)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"productId\" : 1, \"OrderStatus\" : \"IN_PROGRESS\", \"buyerId\" : 2}"))
+                .andExpect(status().isCreated());
+    }
+}
